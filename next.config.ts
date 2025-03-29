3bd47b532fd52981e6d6288e59e2abf0ef366537
root <forest@mail.do.gpup.date>
2025-03-29 14:08:57 -0400

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   async headers() {
     return [
@@ -20,3 +22,5 @@ const nextConfig: NextConfig = {
     ];
   },
 };
+
+export default nextConfig;
